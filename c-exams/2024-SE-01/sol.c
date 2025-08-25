6305d448fe7b733e130e72c860544d4e89cc2a2b
Kaloyan Kolev <kaloyan_tihomirov@icloud.com>
2025-08-26 01:12:07 +0300

    new c exam problem draft solution

---
+++ c-exams/2024-SE-01/sol.c
@@ -0,0 +1,64 @@
+#include <unistd.h>
+
+typedef struct node {
+    uint64_t next;
+    uint8_t u_data[504];
+} node;
+
+bool contains(int fd, uint64_t val) {
+    uint64_t num;
+    if(lseek(fd, 0, SEEK_SET) < 0) { err(9, "cannot lseek"); }
+    ssize_t read_bytes = 0;
+    while ((read_bytes = read(fd, &num, sizeof(num))) == sizeof(num)) {
+        if(num == val) {
+            return true;
+        }
+    }
+
+    if(read_bytes < 0) { err(10, "cannot read"); }
+    return false;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) { errx(1, "incorrect usage"); }
+    //printf("size of struct: %ld\n", sizeof(node));
+    int fd = open(argv[1], O_RDWR);
+    if (fd < 0) { err(2, "cannot open file"); }
+    struct stat f;
+    if(fstat(fd, &f) < 0) { err(3, "cannot stat file"); }
+    if(f.st_size % sizeof(node) != 0) { errx(4, "incorrect input file format"); }
+    
+    ssize_t read_bytes = 0;
+    node c_node;
+    uint64_t curr = 0;
+    char tmp[] = "XXXXXX";
+    int tmp_fd = mkstemp(temp);
+    if (tmp_fd < 0) { err(5, "cannot create tmp_file"); }
+    while((read_bytes = read(fd, &c_node, sizeof(c_node))) > 0) {
+        //printf("next: %ld\n", c_node.next);
+        if (write(tmp_fd, &curr, sizeof(curr)) != sizeof(curr)) { err(6, "cannot write"); }
+        if (c_node.next == 0) {
+            break;
+        }
+        if(c_node.next * sizeof(struct) >= f.st_size) { warnx(8, "referring to non-existing byte, check format"); break; }
+        if(lseek(fd, c_node.next * sizeof(struct), SEEK_SET) < 0) { err(7, "cannot lseek"); }
+        curr = c_node.next;
+    }
+
+    if(read_bytes < 0) { err(8, "cannot read"); }
+
+    size_t records_count = f.st_size / sizeof(node);
+    for (uint64_t i = 1; i < records_count; i++) {
+        if(!contains(tmp_fd, i)) {
+            // write 512B nulls starting from byte with num i * sizeof(node)
+            if(lseek(fd, i * sizeof(node), SEEK_SET) < 0) { err(11, "cannot lseek"); }
+            uint8_t zeros[512] = {0};
+            if(write(fd, &zeros, sizeof(zeros)) != sizeof(zeros)) { err(12, "cannot write"); }
+        }
+    }
+
+    close(fd);
+    close(tmp_fd);
+    unlink(tmp);
+    exit(0);
+}
