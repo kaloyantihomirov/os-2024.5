f367c6a1ff8b82c7d5224363b84751cb4eda8081
Kaloyan Kolev <kaloyan_tihomirov@icloud.com>
2025-08-26 18:34:16 +0300

    fix a bug

--- c-exams/2024-SE-01/sol.c
+++ c-exams/2024-SE-01/sol.c
@@ -1,4 +1,14 @@
+#define _GNU_SOURCE
+#include <stdint.h>
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdlib.h>
 
 typedef struct node {
     uint64_t next;
@@ -32,24 +42,24 @@ int main(int argc, char* argv[]) {
     node c_node;
     uint64_t curr = 0;
     char tmp[] = "XXXXXX";
-    int tmp_fd = mkstemp(temp);
+    int tmp_fd = mkstemp(tmp);
     if (tmp_fd < 0) { err(5, "cannot create tmp_file"); }
+    size_t records_count = f.st_size / sizeof(node);
     while((read_bytes = read(fd, &c_node, sizeof(c_node))) > 0) {
         //printf("next: %ld\n", c_node.next);
         if (write(tmp_fd, &curr, sizeof(curr)) != sizeof(curr)) { err(6, "cannot write"); }
-        if (c_node.next == 0) {
+        if (c_node.next == 0 || c_node.next >= records_count) {
             break;
         }
-        if(c_node.next * sizeof(struct) >= f.st_size) { warnx(8, "referring to non-existing byte, check format"); break; }
-        if(lseek(fd, c_node.next * sizeof(struct), SEEK_SET) < 0) { err(7, "cannot lseek"); }
+        if(lseek(fd, c_node.next * sizeof(c_node), SEEK_SET) < 0) { err(7, "cannot lseek"); }
         curr = c_node.next;
     }
 
     if(read_bytes < 0) { err(8, "cannot read"); }
 
-    size_t records_count = f.st_size / sizeof(node);
     for (uint64_t i = 1; i < records_count; i++) {
         if(!contains(tmp_fd, i)) {
+            printf("will zero out %ld entry\n", i);
             // write 512B nulls starting from byte with num i * sizeof(node)
             if(lseek(fd, i * sizeof(node), SEEK_SET) < 0) { err(11, "cannot lseek"); }
             uint8_t zeros[512] = {0};
